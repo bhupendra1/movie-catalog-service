300b892c415114038a8274b3db933395fc727bfb
bhupendra karki <bhupendra.karki@yahoo.com>
2021-09-26 16:55:10 -0500

    Added Netflix's Hystrix library to make this service fault tolerant

--- src/main/java/com/bhupendra/karki/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/bhupendra/karki/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,13 +2,18 @@ package com.bhupendra.karki.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogServiceApplication {
 
 	public static void main(String[] args) {
@@ -18,6 +23,8 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(3000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 }
--- src/main/java/com/bhupendra/karki/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/bhupendra/karki/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,8 +1,9 @@
 package com.bhupendra.karki.moviecatalogservice.resources;
 
 import com.bhupendra.karki.moviecatalogservice.models.CatalogItem;
-import com.bhupendra.karki.moviecatalogservice.models.Movie;
 import com.bhupendra.karki.moviecatalogservice.models.UserRating;
+import com.bhupendra.karki.moviecatalogservice.services.MovieInfo;
+import com.bhupendra.karki.moviecatalogservice.services.UserRatingInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,18 +20,18 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private MovieInfo movieInfo;
+
+    @Autowired
+    private UserRatingInfo userRatingInfo;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-
-        UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
-
-
-        return userRating.getUserRating().stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
-            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
-        }).collect(Collectors.toList());
-
+        UserRating userRating = userRatingInfo.getUserRating(userId);
+        return userRating.getUserRating().stream().map(rating -> movieInfo.getCatalogItem(rating)).collect(Collectors.toList());
     }
+
 }
 
 
